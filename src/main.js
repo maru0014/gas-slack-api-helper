ec248cd56d6f01a9ac19757e1a376b6eca00b0ef
marumo <maru0014@gmail.com>
2021-06-21 00:48:34 +0900

    Add JSON Converter

--- src/main.js
+++ src/main.js
@@ -133,6 +133,58 @@ class SlackClient {
  * @param {string} token Slackアプリトークン
  * @return {SlackClient}
  */
-function init(token) {
+const init = (token) => {
   return new SlackClient(token);
+};
+
+/**
+ * オブジェクト配列をテーブル配列に変換
+ * @param {Array} obj オブジェクト配列
+ * @return {Array} テーブル化された配列
+ */
+const object2Table = (obj) => {
+  const headers = [Object.keys(obj[0])];
+  const body = obj.map((e) => Object.values(e));
+  const table = headers.concat(body);
+
+  return table;
+};
+
+/**
+ * テーブル配列をオブジェクト配列に変換
+ * @param {Array} table テーブル配列
+ * @return {Array} オブジェクト化された配列
+ */
+const table2Object = (table) => {
+  const arry = [];
+  for (let i = 1; i < table.length; i++) {
+    const obj = {};
+    for (let ii = 0; ii < table[i].length; ii++) {
+      obj[table[0][ii]] = table[i][ii];
+    }
+    arry.push(obj);
+  }
+  return arry;
+};
+
+/**
+ * ネストされたオブジェクトをフラットにする
+ * @param {Object} obj ネストされたオブジェクト
+ * @return {Object} フラット化されたオブジェクト
+ */
+const flattenObj = (obj) => {
+  const result = {};
+  for (const key in obj) {
+    const value = obj[key];
+    if (typeof value === "object") {
+      const flatObj = flattenObj(value);
+      for (const subKey in flatObj) {
+        result[`${key}.${subKey}`] = flatObj[subKey];
+      }
+    } else {
+      result[key] = value;
+    }
 }
+
+  return result;
+};
